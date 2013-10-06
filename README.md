92380c00011bc6a50bf0ba680e920eb8dd0d6d26
eush77 <eush77@gmail.com>
2013-10-07 01:32:55 +0400

    Progress meter

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Habrahabr: Post Length
 =============================
-Prepends window title with post length. This aims to help in comparing different posts with each other.
+Prepends window title with the length of the post. This aims to help in comparing different posts with each other.
 
-`Lorem Ipsum: Dolor Sit Amet` turns into `[5381] Lorem Ipsum: Dolor Sit Amet`.
+For example, `Lorem Ipsum: Dolor Sit Amet` turns into `[5381] Lorem Ipsum: Dolor Sit Amet`.
+
+The script also creates the progress meter fixed nearby the left edge of the screen. It shows the position of the screen center within the post content (e.g. 90% shows that the post is going to end very soon).
--- habrahabr-post-length.user.js
+++ habrahabr-post-length.user.js
@@ -1,13 +1,43 @@
 // ==UserScript==
 // @name        Habrahabr: Post Length
-// @version     0.1
-// @description Prepends window title with post length.
+// @version     1.0
+// @description Augments window title with the post length and inserts progress meter into the page itself.
 // @include     http://habrahabr.ru/post/*/
 // @include     http://habrahabr.ru/company/*/blog/*/
 // ==/UserScript==
 
 !function() {
-    var post = document.getElementsByClassName('post')[0];
-    var length = document.getElementsByClassName('infopanel_wrapper')[0].offsetTop;
-    document.title = '[' + length + '] ' + document.title;
+    var header = document.getElementById('header');
+    var computeLength = new function() {
+        var originalTitle = document.title;
+        var infopanel = document.getElementsByClassName('infopanel_wrapper')[0];
+        return function() {
+            var length = infopanel.offsetTop - header.offsetHeight;
+            // Augment page title
+            document.title = '[' + length + '] ' + originalTitle;
+            return length;
+        };
+    }();
+    computeLength();
+    // Create indicator element
+    var indicator = (function() {
+        var div = document.createElement('div');
+        div.style.position = 'fixed';
+        div.style.left = 0;
+        div.style.bottom = '50%';
+        div.style.paddingLeft = '2px';
+        return div;
+    }());
+    var updateIndicator = new function() {
+        var clientHeight = document.documentElement.clientHeight;
+        return function() {
+            var position = window.pageYOffset + clientHeight / 2 - header.offsetHeight;
+            var progress = Math.floor(position / computeLength() * 100);
+            indicator.innerText = progress <= 100 ? progress + '%' : '';
+        };
+    }();
+    // Set the indicator up
+    updateIndicator();
+    document.body.appendChild(indicator);
+    document.addEventListener('scroll', updateIndicator);
 }();
